8d1653b2edb7e00095d266e6321fbb028a5776fd
Qona'ah Nurussalamah <nurussalamahqonaah@student.uns.ac.id>
2020-03-23 15:23:15 +0700

    tambah file mainThread.java

---
+++ mainThread.java
@@ -0,0 +1,32 @@
+package com.d3ti.pboLanjut20.thread;
+
+public class mainThread {
+	public void jalan(String name) {
+	for(int i=0; i<5; i++) {
+		try {
+			Thread.sleep(1000);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Thread: "+name+" posisi: "+i);
+	}
+	}
+
+}
+public class ThreadInterface extends mainThread implements Runnable{
+	String name;
+	public ThreadInterface(String name){
+		this.name=name;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		jalan(name);
+	}
+	public static void main(String args[]){
+		Thread vespa = new Thread(new ThreadInterface("vespa"));
+		Thread sepeda = new Thread(new ThreadInterface("sepeda"));
+		vespa.start();
+		sepeda.start();
+	}
+}
